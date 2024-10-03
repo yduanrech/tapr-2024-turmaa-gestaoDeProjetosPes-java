b4d03dcbf68c1ca36843376848432d0ef4672ae0
Yduan Rech Effting <105087789+yduanrech@users.noreply.github.com>
2024-10-03 00:29:53 +0000

    first

---
+++ microservgestaoprojetospes/src/main/java/br/univille/microservgestaoprojetospes/MicroservgestaoprojetospesApplication.java
@@ -0,0 +1,13 @@
+package br.univille.microservgestaoprojetospes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservgestaoprojetospesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservgestaoprojetospesApplication.class, args);
+	}
+
+}
---
+++ microservgestaoprojetospes/src/test/java/br/univille/microservgestaoprojetospes/MicroservgestaoprojetospesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.microservgestaoprojetospes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservgestaoprojetospesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
